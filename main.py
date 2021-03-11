c8f98422aa56e221dc7ff40bf3580cb8d06d986a
Walkey37 <pandan2006@mail.ru>
2021-03-11 18:12:40 +0300

    Just test app

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect
+from login_form import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='Домашняя страница')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
