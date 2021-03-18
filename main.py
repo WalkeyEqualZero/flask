c17832e149975b295d6f98cade3fcc4a1df6ce9f
Walkey37 <pandan2006@mail.ru>
2021-03-18 18:13:45 +0300

    registration and more

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, redirect
 from login_form import LoginForm
+from data import db_session
+from data.users import User
+from data.news import News
+import datetime
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -8,8 +13,9 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/')
 @app.route('/index')
 def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='Домашняя страница')
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -20,5 +26,32 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    db_sess = db_session.create_session()
     app.run(port=8080, host='127.0.0.1')
